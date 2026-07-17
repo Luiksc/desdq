8968d7306ec8f4db4ec5fca35106b6398b524625
Luiksc <axelrolon84@gmail.com>
2026-07-17 01:12:02 -0300

    retomando
    retomando cinematica 1 de nvl 2

---
+++ escenarios/nivel 2/cinematicas/cinamatica_n_2.gd
@@ -0,0 +1,83 @@
+extends Node3D
+
+@onready var cine = $AnimationPlayer
+@onready var dio =$dio
+@onready var transi =$"Control/ã/AnimationPlayer"
+@onready var time = $Timer
+@onready var camara =$Camera3D
+@onready var carto =$Control/TextureRect/AnimationPlayer
+
+var fino = false
+
+var dialogo_activo: bool = false
+var npc_actual: String = ""
+var dialogos ={
+	"conversa":[
+		["Dionisio","Marta, Nereñenoiti ra'e.
+(Marta, aún no te acostasta había sido."],
+		["Marta","¡Dionisio!, Ne'ira ndajarreglapai la héfe sombréro.
+(¡Dionisio!, todavía no arregle todo el sombrero del jefe.)"],
+		["Marta","Chekane'õiterei, ko este día rojara y elisa ndive, roipiro mandi'o ha rojarregla sombrerokuéra
+(Estoy muy cansada, hoy acarreamos agua con Elisa, pelamos mandiocas y arreglamos sombreros.)"],
+		["Dionisio", "Erumi ápe, che aterminata péa.
+(Pasame voy a terminar por vos eso.)"],
+		["Marta", "¿Ndikatúiko jasyryry ka'aguýre ha ñasẽ ko añakuágui?
+(¿No podemos escabullirnos por la selva para salir de esta fosa?.)"],
+		["Dionisio", "Peligróso upéa, oje'e Alfredo okañy akokuehe ha ojetopa hete Parana rembe'ýre.
+(Eso es peligroso, dicen que Alfredo se escapo anteayer y encontraron su cuerpo a orillas del río Paraná.)"],
+		["Dionisio", "Ag̃aite atopáta la forma ñasẽ ko'águi.
+(Pronto voy a encontrar como salir de acá)"]
+],
+	"Funcionario":[
+		["Funcionario Municipal","e"],
+		["Dionisio","Erumi ápe ne sombréro, che atermináta péa 
+(Pasame tu sombrero, yo terminon por vos,)"],
+	],
+		
+}
+
+func _ready() -> void:
+	transi.play("salida")
+	time.start()
+	cine.play("dio va")
+	await time.timeout
+	dio.oho = true
+	await cine.animation_finished
+	dio.oho=false
+	camara.position = $posi_camara.position
+	inicia_dialoga("conversa")
+	DialogSystem.dialogo_opa.connect(dialog_terminado)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if dialogo_activo:
+		if Input.is_action_just_pressed("interaccion"):
+			DialogSystem.neixt()
+			$DialogSystem/sound.play()
+	if fino:
+		if Input.is_action_just_pressed("interaccion"):
+			
+
+func inic_dialo() -> void:
+	if npc_actual=="":
+		return
+	if not dialogos.has(npc_actual):
+		return
+	dialogo_activo = true
+	for linea in dialogos[npc_actual]:
+		DialogSystem.says(linea[1], linea[0])
+
+func dialog_terminado() -> void:
+	dialogo_activo = false
+	if npc_actual == "conversa":
+		transi.play("entrafa")
+		carto.play("aparece")
+		fino = true
+		return
+func inicia_dialoga(id_npc: String) -> void:
+	npc_actual = id_npc
+	inic_dialo()
+	
+	
--- escenarios/nivel 2/cinematicas/cinematica_despensa/Dionisio.gd
+++ escenarios/nivel 2/cinematicas/cinematica_despensa/Dionisio.gd
@@ -4,3 +4,8 @@ extends Node3D
 @onready var dialogost = $"../DialogSystem"
 # Called when the node enters the scene tree for the first time.
 var oho = false
+func _process(delta: float) -> void:
+	if oho:
+		anima.play("corre")
+	else:
+		anima.play("repira")
